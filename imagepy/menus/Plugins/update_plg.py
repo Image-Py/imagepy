2b5f944b53489150efbd2e05b670fb49ab5da43a
yxdragon <imagepy@sina.com>
2017-09-28 11:33:51 +0800

    update self

--- imagepy/menus/Plugins/update_plg.py
+++ imagepy/menus/Plugins/update_plg.py
@@ -1,15 +1,23 @@
 from dulwich.repo import Repo
-from dulwich.porcelain import pull
+from dulwich.client import get_transport_and_path
 from imagepy import IPy
 from imagepy.core.engine import Free
 from io import StringIO
 
-class Plugin(Free):
+class Update(Free):
     title = 'Update Software'
 
     def run(self, para=None):
+        IPy.set_info('update now, waiting...')
         repo = Repo('../')
-        output = StringIO()
-        pull(repo, 'https://github.com/Image-Py/imagepy.git')
-        repo.close()
-        #IPy.write(output.read())
+        client, remote_path = get_transport_and_path('https://github.com/Image-Py/imagepy.git')
+        a = client.fetch(remote_path, repo)
+        IPy.alert('imagepy update done!')
+
+class Refresh(Free):
+    title = 'Reload Plugins'
+
+    def run(self, para=None):
+        IPy.curapp.reload_plugins()
+
+plgs = [Update, Refresh]
