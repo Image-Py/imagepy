c3b8c101b082c031f472a992159e4e680ca648be
yxdragon <imagepy@sina.com>
2017-09-28 11:52:01 +0800

    update self

--- imagepy/menus/Plugins/update_plg.py
+++ imagepy/menus/Plugins/update_plg.py
@@ -1,23 +1,29 @@
-from dulwich.repo import Repo
-from dulwich.client import get_transport_and_path
 from imagepy import IPy
 from imagepy.core.engine import Free
-from io import StringIO
 
 class Update(Free):
-    title = 'Update Software'
+	title = 'Update Software'
+	def load(self):
+		try:
+			from dulwich.repo import Repo
+			from dulwich.client import get_transport_and_path
+			self.repo, self.trans = Repo, get_transport_and_path
+		except:
+			IPy.alert('dulwich is needed, you can use Plugins > Install > Install Packages:\ndulwich --global-option="--pure" to install')
+			return False
+		return True
 
-    def run(self, para=None):
-        IPy.set_info('update now, waiting...')
-        repo = Repo('../')
-        client, remote_path = get_transport_and_path('https://github.com/Image-Py/imagepy.git')
-        a = client.fetch(remote_path, repo)
-        IPy.alert('imagepy update done!')
+	def run(self, para=None):
+		IPy.set_info('update now, waiting...')
+		repo = self.repo('../')
+		client, remote_path = self.trans('https://github.com/Image-Py/imagepy.git')
+		a = client.fetch(remote_path, repo)
+		IPy.alert('imagepy update done!')
 
 class Refresh(Free):
-    title = 'Reload Plugins'
+	title = 'Reload Plugins'
 
-    def run(self, para=None):
-        IPy.curapp.reload_plugins()
+	def run(self, para=None):
+		IPy.curapp.reload_plugins()
 
 plgs = [Update, Refresh]
