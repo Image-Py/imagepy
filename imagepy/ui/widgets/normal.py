55861243e9e35bfe6e97ceff12fa87fd954014e2
yxdragon <imagepy@sina.com>
2018-03-31 05:12:05 +0800

    floatslider

--- imagepy/ui/widgets/normal.py
+++ imagepy/ui/widgets/normal.py
@@ -1,4 +1,4 @@
-import wx
+import wx, platform
 import numpy as np
 
 class NumCtrl(wx.TextCtrl):
@@ -70,22 +70,27 @@ class ColorCtrl(wx.TextCtrl):
 class FloatSlider(wx.Panel):
     
     def __init__( self, parent, rang, accury):
+        self.linux = platform.system() == 'Linux'
         wx.Panel.__init__ ( self, parent, id = wx.ID_ANY, pos = wx.DefaultPosition, size = wx.Size( -1,-1 ), style = wx.TAB_TRAVERSAL )
         sizer = wx.BoxSizer( wx.HORIZONTAL )
-        self.slider = wx.ScrollBar( self, wx.ID_ANY, wx.DefaultPosition, wx.Size( -1,25 ), wx.SB_HORIZONTAL )
-        sizer.Add( self.slider, 1, wx.ALL, 0 )
+        if self.linux:
+            self.slider = wx.ScrollBar( self, wx.ID_ANY, wx.DefaultPosition, wx.Size( -1,25 ), wx.SB_HORIZONTAL )
+        else: self.slider = wx.Slider( self, wx.ID_ANY, 128, 0, 255, wx.DefaultPosition, wx.DefaultSize, wx.SL_HORIZONTAL|wx.SL_SELRANGE )
+        sizer.Add( self.slider, 1, wx.ALIGN_CENTER|wx.ALL, 0 )
         self.text = wx.TextCtrl( self, wx.ID_ANY, wx.EmptyString, wx.DefaultPosition, wx.Size( 40,-1 ), 0 )
         sizer.Add( self.text, 0, wx.ALIGN_CENTER|wx.ALL, 0 )
-        #self.spin = wx.SpinButton( self, wx.ID_ANY, wx.DefaultPosition, wx.Size(25, 25), 0 )
-        #sizer.Add( self.spin, 0, wx.ALIGN_CENTER|wx.ALL, 0 )
+        if not self.linux:
+            self.spin = wx.SpinButton( self, wx.ID_ANY, wx.DefaultPosition, wx.Size(25, 25), 0 )
+            sizer.Add( self.spin, 0, wx.ALIGN_CENTER|wx.ALL, 0 )
         self.SetSizer( sizer )
         self.Layout()
         
         # Connect Events
         self.slider.Bind( wx.EVT_SCROLL, self.on_scroll )
         self.text.Bind( wx.EVT_KEY_UP, self.on_text )
-        #self.spin.Bind( wx.EVT_SPIN_DOWN, self.on_down )
-        #self.spin.Bind( wx.EVT_SPIN_UP, self.on_up )
+        if not self.linux:
+            self.spin.Bind( wx.EVT_SPIN_DOWN, self.on_down )
+            self.spin.Bind( wx.EVT_SPIN_UP, self.on_up )
         self.set_para(rang, accury)
 
     def Bind(self, z, f):
@@ -97,8 +102,8 @@ class FloatSlider(wx.Panel):
         self.accury = accury
 
     def on_scroll(self, event):
-        n = self.slider.GetThumbPosition()/255.0*(self.max-self.min)+self.min
-        print(n, round(n, self.accury))
+        value = self.slider.GetThumbPosition() if self.linux else self.slider.GetValue()
+        n = value/255.0*(self.max-self.min)+self.min
         self.text.SetValue(str(round(n,self.accury) if self.accury>0 else int(n)))
         self.text.SetBackgroundColour((255,255,255))
         self.f(event)
@@ -122,7 +127,8 @@ class FloatSlider(wx.Panel):
         if not self.text.HasFocus():
             self.text.SetValue(str(round(n,self.accury) if self.accury>0 else int(n)))
         pos = int(round((n-self.min)/(self.max-self.min)*255))
-        self.slider.SetScrollbar(pos, 0, 255, 0)
+        if self.linux:self.slider.SetScrollbar(pos, 0, 255, 0)
+        if not self.linux:self.slider.SetValue(pos)
         
     def GetValue(self):
         sval = self.text.GetValue()
@@ -131,10 +137,8 @@ class FloatSlider(wx.Panel):
         except ValueError:
             return None
         if num<self.min-1e-20 or num>self.max+1e-20:
-            print(num, self.min, self.max)
             return None
         if abs(round(num, self.accury) - num) > 1E-5:
-            print('fffffffffff')
             return None
         return num
 
