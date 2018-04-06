d2bef22bdc02a4aed377ad63eef1ec0334bea481
Prevalenter <4346liuxing>
2018-04-06 22:31:45 +0800

    prevalenter test

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup
 import setuptools, os
-
+#descr
 descr = 'Image process framework based on plugin like imagej, it is esay to glue with scipy.ndimage, scikit-image, opencv, simpleitk, mayavi...and any libraries based on numpy'
 
 def get_data_files():
