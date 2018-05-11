ab30b33129d0fb5366efefda8563c18f1a4a6bfb
Prevalenter <4346liuxing>
2018-05-11 12:36:50 +0800

    add .dat save Signed-off-by: Prevalenter <4346liuxing>

--- imagepy/menus/File/DAT/dat_plgs.py
+++ imagepy/menus/File/DAT/dat_plgs.py
@@ -1,16 +1,18 @@
 from imagepy.core.util import fileio
 import numpy as np
 from imagepy.core.manager import ReaderManager, WriterManager
-
-
-
 def imread(path):
-	# return pydicom.read_file(path, force=True).pixel_array
 	return np.loadtxt(path,dtype=float)
+def imsave(path,img):
+	np.savetxt(path,img)
 ReaderManager.add('dat', imread)
-
+WriterManager.add('dat', imsave)
 class OpenFile(fileio.Reader):
 	title = 'DAT Open'
 	filt = ['DAT']
 
-plgs = [OpenFile]
+class SaveFile(fileio.Writer):
+	title = 'DAT Save'
+	filt = ['DAT']
+
+plgs = [OpenFile,SaveFile]
