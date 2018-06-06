d7b1663fe01b0f4e2e4d6044ea312da6a4cccd01
gaojian <gaojianjack@qq.com>
2018-06-06 10:23:03 +0800

    add white top hat function to erect small bright   noises

--- imagepy/menus/Process/Binary/binary_plgs.py
+++ imagepy/menus/Process/Binary/binary_plgs.py
@@ -9,7 +9,21 @@ Created on Fri Nov 18 22:51:57 2016
 import scipy.ndimage as ndimg
 import numpy as np
 from imagepy.core.engine import Filter
-from skimage.morphology import convex_hull_object
+from skimage.morphology import convex_hull_object, white_tophat,black_tophat
+from skimage.morphology import disk
+
+class WhiteTophat(Filter):
+    """Closing: derived from imagepy.core.engine.Filter """
+    title = 'WhiteTophat'
+    note = ['8-bit', 'auto_msk', 'auto_snap','preview']
+    para = {'disk_Size':3}
+    view = [(int, 'disk_Size', (1,100), 0, 'width', 'pix')]
+
+    def run(self, ips, snap, img, para = None):
+        selem = disk(para['disk_Size']);
+        white_tophat(snap, selem, out=img)
+
+
 
 class Closing(Filter):
     """Closing: derived from imagepy.core.engine.Filter """
@@ -23,7 +37,7 @@ class Closing(Filter):
         strc = np.ones((para['h'], para['w']), dtype=np.uint8)
         ndimg.binary_closing(snap, strc, output=img)
         img *= 255
-        
+
 class Opening(Filter):
     """Opening: derived from imagepy.core.engine.Filter """
     title = 'Binary Opening'
@@ -36,7 +50,7 @@ class Opening(Filter):
         strc = np.ones((para['h'], para['w']), dtype=np.uint8)
         ndimg.binary_opening(snap, strc, output=img)
         img *= 255
-        
+
 class Dilation(Filter):
     """Dilation: derived from imagepy.core.engine.Filter """
     title = 'Binary Dilation'
@@ -49,7 +63,7 @@ class Dilation(Filter):
         strc = np.ones((para['h'], para['w']), dtype=np.uint8)
         ndimg.binary_dilation(snap, strc, output=img)
         img *= 255
-        
+
 class Erosion(Filter):
     """Erosion: derived from imagepy.core.engine.Filter """
     title = 'Binary Erosion'
@@ -62,7 +76,7 @@ class Erosion(Filter):
         strc = np.ones((para['h'], para['w']), dtype=np.uint8)
         ndimg.binary_erosion(snap, strc, output=img)
         img *= 255
-        
+
 class Outline(Filter):
     """Outline: derived from imagepy.core.engine.Filter """
     title = 'Binary Outline'
@@ -72,7 +86,7 @@ class Outline(Filter):
         ndimg.binary_dilation(snap, output=img)
         img *= 255
         img -= snap
-        
+
 class FillHoles(Filter):
     """FillHoles: derived from imagepy.core.engine.Filter """
     title = 'Fill Holes'
@@ -89,6 +103,6 @@ class Convex(Filter):
     #process
     def run(self, ips, snap, img, para = None):
         img[convex_hull_object(snap)] = 255
-        
 
-plgs = [Dilation, Erosion, '-', Closing, Opening, '-', Outline, FillHoles, Convex]
+
+plgs = [Dilation, Erosion, '-', Closing, Opening, '-', WhiteTophat,Outline, FillHoles, Convex]
