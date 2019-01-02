649dac35aed51447f5dabb8a59eafe8fa725fede
yxdragon <imagepy@sina.com>
2019-01-02 22:34:27 +0800

    jpg imageio

--- imagepy/menus/File/JPG/jpg_plgs.py
+++ imagepy/menus/File/JPG/jpg_plgs.py
@@ -1,11 +1,12 @@
 from imagepy.core.util import fileio
-from skimage.io import imread, imsave
+from imageio import imread, imsave
 from imagepy.core.manager import ReaderManager, WriterManager
 
 ReaderManager.add('jpg', imread)
 WriterManager.add('jpg', imsave)
 ReaderManager.add('jpeg', imread)
 WriterManager.add('jpeg', imsave)
+
 class OpenFile(fileio.Reader):
 	title = 'JPG Open'
 	filt = ['JPG','JPEG']
