5d5bb36be570592cb59981bf9cdf8e5aca9aad4c
yxdragon <imagepy@sina.com>
2019-03-01 11:57:54 +0800

    stackreg

---
+++ imagepy/menus/Plugins/StackReg/StackReg License.md
@@ -0,0 +1,12 @@
+# Gratitude for StackReg
+
+You'll be free to use this software for research purposes, but you
+should not redistribute it without our consent. In addition, we expect
+you to include a citation or acknowledgment whenever you present or
+publish results that are based on it.
+
+**Additional help available at [http://bigwww.epfl.ch/thevenaz/turboreg](http://bigwww.epfl.ch/thevenaz/turboreg/)**
+
+![befor](http://bigwww.epfl.ch/thevenaz/turboreg/before.gif)
+![after](http://bigwww.epfl.ch/thevenaz/turboreg/after.gif)
+
--- imagepy/menus/Plugins/StackReg/stackreg_plgs.py
+++ imagepy/menus/Plugins/StackReg/stackreg_plgs.py
@@ -4,32 +4,52 @@ from pystackreg import StackReg
 import numpy as np
 import pandas as pd
 from skimage import transform as tf
+import scipy.ndimage as ndimg
 from imagepy.core.manager import TableManager
 
 class Register(Simple):
 	title = 'Stack Register'
 	note = ['8-bit', '16-bit', 'int', 'float', 'stack']
 
-	para = {'trans':'RIGID_BODY', 'ref':'previous', 'tab':False, 'new':'Inplace'}
+	para = {'trans':'RIGID_BODY', 'ref':'previous', 'tab':False, 'new':'Inplace', 'diag':0, 'sigma':0}
 	view = [(list, 'trans', ['TRANSLATION', 'RIGID_BODY', 'SCALED_ROTATION', 'AFFINE', 'BILINEAR'], str,  'transform', ''),
 			(list, 'ref', ['previous', 'first', 'mean'], str, 'reference', ''),
 			(list, 'new', ['Inplace', 'New', 'None'], str, 'image', ''),
+			(int, 'diag', (0, 2048), 0, 'diagonal', 'scale'),
+			(float, 'sigma', (0,30), 1, 'sigma', 'blur'),
 			(bool, 'tab', 'show table')]
 
 	def run(self, ips, imgs, para = None):
-		news = np.array(imgs)
+		k = para['diag']/np.sqrt((np.array(ips.img.shape)**2).sum())
+		size = tuple((np.array(ips.img.shape)*k).astype(np.int16))
+		IPy.set_info('down sample...')
+		news = []
+		for img in imgs:
+			if k!=0: img = tf.resize(img, size)
+			if para['sigma']!=0:
+				img = ndimg.gaussian_filter(img, para['sigma'])
+			news.append(img)
+
+		IPy.set_info('register...')
 		sr = StackReg(eval('StackReg.%s'%para['trans']))
-		sr.register_stack(news, reference=para['ref'])
+		sr.register_stack(np.array(news), reference=para['ref'])
+
 		mats = sr._tmats.reshape((sr._tmats.shape[0],-1))
+		if k!=0: mats[:,[0,1,3,4,6,7]] *= k
+		if k!=0: mats[:,[0,1,2,3,4,5]] /= k
+
 		if para['tab']: IPy.show_table(pd.DataFrame(
 			mats, columns=['A%d'%(i+1) for i in range(mats.shape[1])]), title='%s-Tmats'%ips.title)
+
 		if para['new'] == 'None': return
+		IPy.set_info('transform...')
 		for i in range(sr._tmats.shape[0]):
 			tform = tf.ProjectiveTransform(matrix=sr._tmats[i])
 			if para['new'] == 'Inplace':
 				imgs[i] = tf.warp(imgs[i], tform)*imgs[i].max()
 			if para['new'] == 'New':
 				news[i] = tf.warp(imgs[i], tform)*imgs[i].max()
+			self.progress(i, len(imgs))
 		if para['new'] == 'New': IPy.show_img(news, '%s-reg'%ips.title)
 
 class Transform(Simple):
@@ -46,12 +66,18 @@ class Transform(Simple):
 			IPy.alert('image stack must has the same length as transfrom mats!')
 			return
 		newimgs = []
+		img = np.zeros_like(ips.img, dtype=np.float64)
 		for i in range(len(mats)):
 			tform = tf.ProjectiveTransform(matrix=mats[i].reshape((3,3)))
-			img = tf.warp(imgs[i], tform)
+			if imgs[i].ndim==2: 
+				img[:] = tf.warp(imgs[i], tform)
+			else:
+				for c in range(img.shape[2]):
+					img[:,:,c] = tf.warp(imgs[i][:,:,c], tform)
 			img -= imgs[i].min(); img *= imgs[i].max() - imgs[i].min()
-			if para['new']: newimgs.append(img)
+			if para['new']: newimgs.append(img.astype(ips.img.dtype))
 			else: imgs[i] = img
+			self.progress(i, len(mats))
 		if para['new']: IPy.show_img(newimgs, '%s-trans'%ips.title)
 
 plgs = [Register, Transform]
