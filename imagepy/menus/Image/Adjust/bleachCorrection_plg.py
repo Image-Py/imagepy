634b3cde2a8a65ef532e6ef95a78c7434f717141
weisongzhao <zhaoweisong950713@163.com>
2020-01-25 15:25:49 +0800

    bleach table and plot

--- imagepy/menus/Image/Adjust/bleachCorrection_plg.py
+++ imagepy/menus/Image/Adjust/bleachCorrection_plg.py
@@ -8,14 +8,20 @@ from imagepy import IPy
 from scipy.optimize import curve_fit
 # from .histogram_plg import like
 from skimage.exposure import histogram_matching
+import matplotlib.pyplot as plt 
+import pandas as pd
 
 def simple_ratio(imgs):
     x,y,z = imgs.shape
     ref = imgs[:,:,1].sum()/x/y
+    ratio=np.zeros([z,2])
+    ratio[0,0]=ref
+    ratio[0,1]=1
     for i in range(z-1):
-        imgs[:,:,i+1] *= (ref/(imgs[:,:,i+1].sum()
-            /x/y)).astype(imgs.dtype)
-    return imgs
+        ratio[i+1,0]=(imgs[:,:,i+1].sum()/x/y)
+        ratio[i+1,1]=ref/(imgs[:,:,i+1].sum()/x/y)
+        imgs[:,:,i+1] *= ratio[i+1,1].astype(imgs.dtype)
+    return imgs, ratio
 
 def exponential_fit(imgs):
     imgs_=imgs.astype('float32')
@@ -27,7 +33,7 @@ def exponential_fit(imgs):
         ratio=exponential_func(0, popt[0], popt[1], popt[2])/ \
         exponential_func(i+1, popt[0], popt[1], popt[2])
         imgs_[:,:,i+1] *= ratio
-    return imgs_.astype(imgs.dtype)
+    return imgs_.astype(imgs.dtype), popt, intensity
 
 def histogram_match(imgs):    
     x,y,z = imgs.shape
@@ -41,6 +47,7 @@ def exponential_func(t, ref, k, offset):
 
 class Plugin(Simple):
     title = 'Bleach Correction'
+    asyn = False
     note = ['8-bit','16-bit','stack']
     para = {'Method':'Simple ratio', 'Background':0}
     view = [(list, 'Method', 
@@ -57,10 +64,24 @@ class Plugin(Simple):
             imgs_ = np.maximum(imgs_, 0)
         print (para['Method'])
         if para['Method'] == 'Simple ratio':
-            imgs_=simple_ratio(imgs_)
+            imgs_,ratio = simple_ratio(imgs_)
+            t = np.linspace(0, imgs_.shape[2]-1, imgs_.shape[2])
+            index = ['Frame%s'%i for i in range(1,imgs_.shape[2]+1)]
+            columns = ['Mean value', 'Ratio']
+            IPy.show_table(pd.DataFrame(ratio, index, columns), 'Log of simple ratio')
+
             # IPy.show_img(imgs_.transpose(2,0,1),'Corrected %s'%ips.title)
         if para['Method'] == 'Exponential fit':
-            imgs_=exponential_fit(imgs_)
+            [imgs_, popt, intensity] = exponential_fit(imgs_)
+            t = np.linspace(0, imgs_.shape[2]-1, imgs_.shape[2])
+            fitresult = exponential_func(t, popt[0], popt[1], popt[2])
+            plt.plot(t,intensity,'r.',label='Experiment')
+            plt.plot(t,fitresult,'k',label=
+            'Exponential fitted curve\n y=a*exp(-bx)+c\n a=%f\n b=%f\n c=%f'
+                %(popt[0],popt[1],popt[2]))
+            plt.title('Exponential fitted result')
+            plt.legend()
+            plt.show()
             # IPy.show_img(imgs_.transpose(2,0,1),'Corrected %s'%ips.title)
         if para['Method'] == 'Histogram match':
             imgs_=histogram_match(imgs_)
