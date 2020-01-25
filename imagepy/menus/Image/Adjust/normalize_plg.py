32d2592f032bc398680175b6d4b42729441cf9dc
weisongzhao <zhaoweisong950713@163.com>
2020-01-25 16:16:18 +0800

    normalize

---
+++ imagepy/menus/Image/Adjust/normalize_plg.py
@@ -0,0 +1,35 @@
+"""
+Created on Sun Jan 23 11:53:00 2020
+@author: weisong
+"""
+from imagepy.core.engine import Simple
+import numpy as np
+from imagepy import IPy
+
+class Plugin(Simple):
+    title = 'Normalize'
+    note = ['8-bit','16-bit','int','float','stack']
+    para = {'if3d': False, 'Sb':False}
+    view = [(bool, 'if3d', '3D stack'),
+    (bool, 'Sb', 'Subtract background')]
+
+    def run(self, ips, imgs, para = None):
+        # .astype('float64')
+        imgs_= np.array(imgs).astype('float64').transpose(1,2,0)
+        x, y, z = imgs_.shape
+        if para['if3d']:
+            if para['Sb']:
+                imgs_ -= imgs_.min()
+            imgs_ = imgs_ / imgs_.max()
+        else:
+            if para['Sb']:
+                for i in range(z):
+                    imgs_[:,:,i] -= imgs_[:,:,i].min()
+            for i in range(z):
+                imgs_[:,:,i] = imgs_[:,:,i] / imgs_[:,:,i].max()        
+        if imgs.dtype == np.uint8:
+            imgs[:] = (255*imgs_).astype(imgs.dtype).transpose(2,0,1)
+        elif imgs.dtype == np.uint16:
+            imgs[:] = (65535*imgs_).astype(imgs.dtype).transpose(2,0,1)
+        else:
+            imgs[:] = (imgs_).astype(imgs.dtype).transpose(2,0,1)
