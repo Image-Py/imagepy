cf37cbb679f828d978a56e32186103701af4cb50
weisongzhao <zhaoweisong950713@163.com>
2020-01-25 17:17:48 +0800

    stretch contrast

--- imagepy/menus/Image/Adjust/__init__.py
+++ imagepy/menus/Image/Adjust/__init__.py
@@ -1,1 +1,1 @@
-catlog = ['threshold_plg','graystairs_plg','brightcons_plg','curve_plg','-','bleachCorrection_plg','normalize_plg','-','colorbalance_plg','colorstairs_plg','-','histogram_plgs']
+catlog = ['threshold_plg','graystairs_plg','brightcons_plg','curve_plg','-','bleachCorrection_plg','enhanceContrast_plg','normalize_plg','-','colorbalance_plg','colorstairs_plg','-','histogram_plgs']
