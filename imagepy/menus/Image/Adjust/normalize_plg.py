0f609a8ea47ea5601949402ca5fcc1fbc2abc61c
weisongzhao <zhaoweisong950713@163.com>
2020-01-29 11:04:54 +0800

    normalize for list support

--- imagepy/menus/Image/Adjust/normalize_plg.py
+++ imagepy/menus/Image/Adjust/normalize_plg.py
@@ -14,20 +14,20 @@ class Plugin(Simple):
     (bool, 'Sb', 'Subtract background')]
 
     def run(self, ips, imgs, para = None):
-        imgs_= np.array(imgs).astype('float64').transpose(1,2,0)
+        imgs_=imgs.astype('float64')
         if para['if3d']:
             if para['Sb']:
-                imgs_ -= imgs_.min()
+                 imgs_ -= imgs_.min()
             imgs_ = imgs_ / imgs_.max()
         else:
             if para['Sb']:
-                for i in range(z):
-                    imgs_[:,:,i] -= imgs_[:,:,i].min()
-            for i in range(z):
-                imgs_[:,:,i] = imgs_[:,:,i] / imgs_[:,:,i].max()        
+                 for i in range(len(imgs_)):
+                     imgs_[i] -= imgs_[i].min()
+            for i in range(len(imgs_)):
+                imgs_[i] = imgs_[i] / imgs_[i].max()
         if imgs.dtype == np.uint8:
-            imgs[:] = (255*imgs_).astype(imgs.dtype).transpose(2,0,1)
+            imgs[:] = (255*imgs_).astype(imgs.dtype)
         elif imgs.dtype == np.uint16:
-            imgs[:] = (65535*imgs_).astype(imgs.dtype).transpose(2,0,1)
+            imgs[:] = (65535*imgs_).astype(imgs.dtype)
         else:
-            imgs[:] = (imgs_).astype(imgs.dtype).transpose(2,0,1)
+            imgs[:] = (imgs_).astype(imgs.dtype)
